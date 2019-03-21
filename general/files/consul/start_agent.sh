c8a208ffbd14003f4f7b775a34de1e641c4222df
Maarten Mol <maartenmol96@gmail.com>
2019-03-21 22:45:33 +0100

    Fix consul services

--- general/files/consul/start_agent.sh
+++ general/files/consul/start_agent.sh
@@ -1,2 +1,2 @@
-ip=$(dig +short `hostname -f`)
+ip=$(python -c 'import socket; print socket.gethostbyname(socket.getfqdn())')
 /usr/local/bin/consul agent -config-dir=/etc/consul.d/client -rejoin -ui -data-dir=/var/db/consul -bind=${ip}
